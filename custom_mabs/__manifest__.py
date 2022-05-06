ecd3a353179a370a18f460aee69a52588ba67de7
pedroguirao <pedro@serincloud.com>
2022-05-06 10:44:34 +0200

    mabs

---
+++ custom_mabs/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#    License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+#    Copyright (C) 2021 Serincloud S.L. All Rights Reserved
+#    PedroGuirao pedro@serincloud.com
+##############################################################################
+
+{
+    "name": "Custom Mabs",
+    "version": "14.0.1.0.0",
+    "category": "Contracts/Purchases",
+    "author": "Serincloud",
+    "maintainer": "Serincloud",
+    "website": "www.ingenieriacloud.com",
+    "license": "AGPL-3",
+    "depends": [
+        "purchase",
+    ],
+    "data": [
+
+    ],
+    "installable": True,
+}
---
+++ custom_mabs/models/purchase_order.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#    License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+#    Copyright (C) 2021 Serincloud S.L. All Rights Reserved
+#    PedroGuirao pedro@serincloud.com
+##############################################################################
+from odoo import api, fields, models, _
+
+
+class ProductTemplate(models.Model):
+    _inherit = "purchase.order"
+
+    legal_partner_id = fields.Many2one('res.partner', string='Representante Legal')
