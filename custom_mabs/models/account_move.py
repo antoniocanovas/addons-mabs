b8bd173bacbe0d347f9b9f5a3cfb2cc5d4ea176f
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2022-05-31 19:49:22 +0200

    donation fields

--- custom_mabs/models/account_move.py
+++ custom_mabs/models/account_move.py
@@ -10,32 +10,4 @@ from odoo import api, fields, models, _
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    donation_invoice_id = fields.Many2one('account.move', string='Donation invoice')
-
-    def _create_donation_invoice(self):
-        for record in self:
-            if not record.donation_invoice_id.id:
-              donation_journal = self.env.company.donation_journal
-              donation_product = self.env.company.donation_product
-              donation_invoice = self.env['account.move'].create({'move_type':'out_invoice',
-                                                                  'journal_id':donation_journal.id,
-                                                                  'partner_id':record.partner_id})
-              record['donation_invoice_id'] = donation_invoice.id
-
-              for li in record.invoice_line_ids:
-                if li.price_subtotal != 0:
-                  name = " (" + str(li.quantity) + " x " + li.product_id.name +")"
-                  donation_invoice['line_ids'] = [(0,0,{
-                        'product_id':donation_product.id,
-                        'name': donation_product.name + name,
-                        'credit': abs(li.price_subtotal),
-                        'account_id': donation_product.property_account_expense_id.id,
-                        'analytic_account_id':li.analytic_account_id.id,
-                        'partner_id': record.partner_id.id
-                     }), (0, 0, {
-                        'name': record.name or '/',
-                        'debit': abs(li.price_subtotal),
-                        'account_id': record.partner_id.property_account_receivable_id.id,
-                        'partner_id': record.partner_id.id,
-                        'exclude_from_invoice_tab':True
-                     })]
+    donation_invoice_id = fields.Many2one('account.move', string='Donation invoice', copy=False)
